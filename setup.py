bd4c17879c15d00e63676d1c12da88b916a564a8
George C. Privon <gprivon@nrao.edu>
2020-07-08 17:46:19 -0400

    v0.2.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pymccorrelation",
-    version="0.2.1",
+    version="0.2.2",
     author="George C. Privon",
     author_email="gprivon@nrao.edu",
     description="Compute correlation coefficients with uncertainties",
