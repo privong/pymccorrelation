415524476914543cb50303ac5f4edcf06dc70df3
George C. Privon <gprivon@nrao.edu>
2021-05-26 15:51:09 -0400

    clean-up for actual v0.2.4 release on pypi

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pymccorrelation",
-    version="0.2.3",
+    version="0.2.4",
     author="George C. Privon",
     author_email="gprivon@nrao.edu",
     description="Compute correlation coefficients with uncertainties",
